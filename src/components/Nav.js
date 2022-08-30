d445455577dadcd280e2be561d27514419a63d88
Adesipe Adeola <princeadeola234@gmail.com>
2022-08-30 04:42:22 -0700

    committed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import '../App.css';
+import {Link} from 'react-router-dom'
 
 
 function Nav() {
-  return (
-    <nav>
-      <h3>ROUTING</h3>
-      <ul className='nav-links'>
-        <li>About</li>
-        <li>Shop</li>
-      </ul>
-    </nav>
-  );
+    const navStyle = {
+        color: 'white',
+        cursor: 'pointer',
+        textDecoration: 'none'
+    }
+
+    return (
+        <nav>
+        <Link to="/" style={navStyle}>
+            <h3>ROUTING</h3>
+        </Link>
+        <ul className='nav-links'>
+            <Link to="/about" style={navStyle}>
+                <li>About</li>
+            </Link>
+            <Link to="/shop" style={navStyle}>
+                <li>Shop</li>
+            </Link>
+        </ul>
+        </nav>
+    );
 }
 
 export default Nav;
